1da32bad574b803605b15709a197908fa776bc7d
Ender Karan <ender.karan14@gmail.com>
2026-01-20 10:40:11 +0300

    css ve tailwind config ayari yapti

--- fm-showcase/tailwind.config.js
+++ fm-showcase/tailwind.config.js
@@ -5,10 +5,19 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
-      fontFamily: {
-      epilogue: ['Epilogue', 'sans-serif'],
+  extend: {
+    fontFamily: {
+      sora: ['Sora', 'sans-serif'], 
+      epilogue: ['Epilogue', 'sans-serif'], 
     },
-    },
+    colors: {
+      'ty-black': 'hsl(0, 0%, 7%)',       
+      'ty-gray-dark': 'hsl(0, 0%, 15%)',  
+      'ty-gray': 'hsl(240, 3%, 46%)',     
+      'ty-gray-light': 'hsl(240, 1%, 59%)', 
+      'ty-primary': 'hsl(49, 85%, 70%)', 
+    }
+  }
+},
     plugins: [],
   }
