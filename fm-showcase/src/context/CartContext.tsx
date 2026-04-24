0a6fb855402065a7438abe2d0c3c4ec0f8cd4a30
EnderKaran <ender.karan14@gmail.com>
2026-04-24 12:02:08 +0300

    card context taslak yapıyı yaptım ve resimleri ekledim

---
+++ fm-showcase/src/context/CartContext.tsx
@@ -0,0 +1,15 @@
+
+interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+
+interface CartContextType {
+  items: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: string) => void;
+  totalItems: number;
+}
